0922caac2716143cdd5bb752ed016537df35fa6e
Alexander Grebenyuk <grebenyuk.alexander@gmail.com>
2015-03-12 15:50:45 -1000

    Add empty project

---
+++ Nuke/Source/Nuke.h
@@ -0,0 +1,19 @@
+//
+//  Nuke.h
+//  Nuke
+//
+//  Created by Alexander Grebenyuk on 3/12/15.
+//  Copyright (c) 2015 Alexander Grebenyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Nuke.
+FOUNDATION_EXPORT double NukeVersionNumber;
+
+//! Project version string for Nuke.
+FOUNDATION_EXPORT const unsigned char NukeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Nuke/PublicHeader.h>
+
+
