3e30af858cbeca28642089f6956467e645ea79f8
kean <grebenyuk.alexander@gmail.com>
2015-10-01 11:49:51 +0300

    ImageMemoryCache recommendedCacheTotalLimit method doesnt require conditional compilation

--- Nuke/Source/Core/ImageMemoryCache.swift
+++ Nuke/Source/Core/ImageMemoryCache.swift
@@ -13,7 +13,7 @@ public protocol ImageMemoryCaching {
 public class ImageCachedResponse {
     public let image: UIImage
     public let userInfo: Any?
-    
+
     public init(image: UIImage, userInfo: Any?) {
         self.image = image
         self.userInfo = userInfo
@@ -22,59 +22,53 @@ public class ImageCachedResponse {
 
 public class ImageMemoryCache: ImageMemoryCaching {
     public let cache: NSCache
-    
+
     deinit {
         #if os(iOS)
             NSNotificationCenter.defaultCenter().removeObserver(self, name: UIApplicationDidReceiveMemoryWarningNotification, object: nil)
         #endif
     }
-    
+
     public init(cache: NSCache) {
         self.cache = cache
         #if os(iOS)
             NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("didReceiveMemoryWarning:"), name: UIApplicationDidReceiveMemoryWarningNotification, object: nil)
         #endif
     }
-    
+
     public convenience init() {
         let cache = NSCache()
         cache.totalCostLimit = ImageMemoryCache.recommendedCacheTotalLimit()
         self.init(cache: cache)
     }
-    
+
     public func cachedResponseForKey(key: AnyObject) -> ImageCachedResponse? {
         let object: AnyObject? = self.cache.objectForKey(key)
         return object as? ImageCachedResponse
     }
-    
+
     public func storeResponse(response: ImageCachedResponse, forKey key: AnyObject) {
         let cost = self.costForImage(response.image)
         self.cache.setObject(response, forKey: key, cost: cost)
     }
-    
+
     public func costForImage(image: UIImage) -> Int {
         let imageRef = image.CGImage
         let bits = CGImageGetWidth(imageRef) * CGImageGetHeight(imageRef) * CGImageGetBitsPerPixel(imageRef)
         return bits / 8
     }
-    
+
     public class func recommendedCacheTotalLimit() -> Int {
-        #if os(iOS)
-            
-            let physicalMemory = Double(NSProcessInfo.processInfo().physicalMemory)
-            let ratio = physicalMemory <= (1024 * 1024 * 512 /* 512 Mb */) ? 0.1 : 0.2
-            
-            let limit = physicalMemory * ratio
-            return limit > Double(Int.max) ? Int.max : Int(limit)
-        #else
-            return 1024 * 1024 * 30 // 30 Mb
-        #endif
+        let physicalMemory = Double(NSProcessInfo.processInfo().physicalMemory)
+        let ratio = physicalMemory <= (1024 * 1024 * 512 /* 512 Mb */) ? 0.1 : 0.2
+        let limit = physicalMemory * ratio
+        return limit > Double(Int.max) ? Int.max : Int(limit)
     }
-    
+
     public func removeAllCachedImages() {
         self.cache.removeAllObjects()
     }
-    
+
     @objc private func didReceiveMemoryWarning(notification: NSNotification) {
         self.cache.removeAllObjects()
     }
