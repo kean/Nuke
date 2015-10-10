4cd2461edea4aa3303215fc4c67936c0403cdec5
kean <grebenyuk.alexander@gmail.com>
2015-10-10 21:19:49 +0300

    Annotate ImageTask abstract methods; Make constructor public

--- Nuke/Source/Core/ImageTask.swift
+++ Nuke/Source/Core/ImageTask.swift
@@ -19,27 +19,27 @@ public class ImageTask: Hashable {
     public internal(set) var response: ImageResponse?
     public lazy var progress = NSProgress(totalUnitCount: -1)
     
-    internal init(request: ImageRequest, identifier: Int) {
+    public init(request: ImageRequest, identifier: Int) {
         self.request = request
         self.identifier = identifier
+    }
+    
+    public var hashValue: Int {
+        return self.identifier
     }
     
     /** Adds completion block to the task. Completion block is called even if it is added to the alredy completed task.
     
     Completion block to be called on the main thread when task is either completed or cancelled. Completion block is called synchronously when the requested image can be retrieved from the memory cache and the request was made from the main thread.
     */
-    public func completion(completion: ImageTaskCompletion) -> Self { return self }
+    public func completion(completion: ImageTaskCompletion) -> Self { fatalError("Abstract method") }
     
-    public var hashValue: Int {
-        return self.identifier
-    }
+    public func resume() -> Self { fatalError("Abstract method") }
     
-    public func resume() -> Self { return self }
-
     /** Advices image task to suspend loading. Suspended task might still complete at any time. A download task can continue transferring data at a later time. All other tasks must start over when resumed. For more info on suspending NSURLSessionTask see NSURLSession documentation.
     */
-    public func suspend() -> Self { return self }
-    public func cancel() -> Self { return self }
+    public func suspend() -> Self { fatalError("Abstract method") }
+    public func cancel() -> Self { fatalError("Abstract method") }
 }
 
 public func ==(lhs: ImageTask, rhs: ImageTask) -> Bool {
