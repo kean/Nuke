c13e927146e1397f7fda409c86ab2746cb31eda7
kean <grebenyuk.alexander@gmail.com>
2015-10-11 16:11:10 +0300

    Add Nuke watchOS target

---
+++ Nuke/Nuke watchOS/Nuke watchOS.h
@@ -0,0 +1,19 @@
+//
+//  Nuke watchOS.h
+//  Nuke watchOS
+//
+//  Created by Alexander Grebenyuk on 11/10/15.
+//  Copyright © 2015 Alexander Grebenyuk. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for Nuke watchOS.
+FOUNDATION_EXPORT double Nuke_watchOSVersionNumber;
+
+//! Project version string for Nuke watchOS.
+FOUNDATION_EXPORT const unsigned char Nuke_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Nuke_watchOS/PublicHeader.h>
+
+
