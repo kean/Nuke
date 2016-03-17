dbe12eb1137aa3fce2cb84df6a00ab73c6a2abe2
kean <grebenyuk.alexander@gmail.com>
2016-03-17 22:14:56 +0300

    Move TaskQueue to Internal.swift

--- Source/Core/Internal.swift
+++ Source/Core/Internal.swift
@@ -34,3 +34,71 @@ extension NSOperationQueue {
         self.maxConcurrentOperationCount = maxConcurrentOperationCount
     }
 }
+
+// MARK: TaskQueue
+
+/// Limits number of concurrent tasks, prevents trashing of NSURLSession
+class TaskQueue {
+    var maxExecutingTaskCount = 8
+    var congestionControlEnabled = true
+    
+    private let queue: dispatch_queue_t
+    private var pendingTasks = NSMutableOrderedSet()
+    private var executingTasks = Set<NSURLSessionTask>()
+    private var executing = false
+    
+    init(queue: dispatch_queue_t) {
+        self.queue = queue
+    }
+    
+    func resume(task: NSURLSessionTask) {
+        if !pendingTasks.containsObject(task) && !executingTasks.contains(task) {
+            pendingTasks.addObject(task)
+            setNeedsExecute()
+        }
+    }
+    
+    func cancel(task: NSURLSessionTask) {
+        if pendingTasks.containsObject(task) {
+            pendingTasks.removeObject(task)
+        } else if executingTasks.contains(task) {
+            executingTasks.remove(task)
+            task.cancel()
+            setNeedsExecute()
+        }
+    }
+    
+    func finish(task: NSURLSessionTask) {
+        if pendingTasks.containsObject(task) {
+            pendingTasks.removeObject(task)
+        } else if executingTasks.contains(task) {
+            executingTasks.remove(task)
+            setNeedsExecute()
+        }
+    }
+    
+    func setNeedsExecute() {
+        if !executing {
+            executing = true
+            if congestionControlEnabled {
+                // Executing tasks too frequently might trash NSURLSession to the point it would crash or stop executing tasks
+                let delay = min(30.0, 8.0 + Double(executingTasks.count))
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_MSEC))), queue) {
+                    self.execute()
+                }
+            } else {
+                execute()
+            }
+        }
+    }
+    
+    func execute() {
+        executing = false
+        if let task = pendingTasks.firstObject as? NSURLSessionTask where executingTasks.count < maxExecutingTaskCount {
+            pendingTasks.removeObjectAtIndex(0)
+            executingTasks.insert(task)
+            task.resume()
+            setNeedsExecute()
+        }
+    }
+}
