9a92e602e3d086a45c39daa2b65c8a11597fe2c4
kean <grebenyuk.alexander@gmail.com>
2016-10-13 19:00:07 +0300

    Add documentation for Promise

--- Sources/Promise.swift
+++ Sources/Promise.swift
@@ -4,40 +4,71 @@
 
 import Foundation
 
-// Promise is built-in into Nuke to avoid external dependencies.
-
+/// A promise is an object that represents an asynchronous task. Use `then(...)` to
+/// get the result of the promise. Use `catch(...)` to catch errors.
+///
+/// Promises start in a *pending* state and *resolve* with a value to become
+/// *fulfilled* or an `Error` to become *rejected*.
+///
+/// A notable `Promise` feature is that it bubbles up errors. It allows you to
+/// catch all errors returned by a chain of promises with a single `catch(...)`.
 public final class Promise<T> {
+    // Promise is built into Nuke to avoid fetching external dependencies.
+
     private var state: PromiseState<T> = .pending(PromiseHandlers<T>())
     private let lock = Lock()
-    
+
+    /// Creates a new, pending promise.
+    ///
+    /// ```
+    /// func loadData(url: URL) -> Promise<Data> {
+    ///     return Promise<Data> { fulfill, reject in
+    ///         URLSession.shared.dataTask(with: url) { data, _, error in
+    ///             if let data = data {
+    ///                 fulfill(data)
+    ///             } else {
+    ///                 reject(error ?? unknownError)
+    ///             }
+    ///         }.resume()
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// - parameter value: The provided closure is called immediately on the
+    /// current thread. In the closure you should start an asynchronous task and
+    /// call either `fulfill` or `reject` when it completes.
     public init(_ closure: (_ fulfill: @escaping (T) -> Void, _ reject: @escaping (Error) -> Void) -> Void) {
-        closure({ self.resolve(resolution: .fulfilled($0)) },
-                { self.resolve(resolution: .rejected($0)) })
+        closure({ self.resolve(.fulfilled($0)) }, { self.resolve(.rejected($0)) })
     }
-    
+
+    /// Creates a promise fulfilled with a given value.
     public init(value: T) {
         state = .resolved(.fulfilled(value))
     }
-    
+
+    /// Create a promise rejected with a given error.
     public init(error: Error) {
         state = .resolved(.rejected(error))
     }
 
-    private func resolve(resolution: PromiseResolution<T>) {
+    private func resolve(_ resolution: PromiseResolution<T>) {
         lock.lock()
-        if case let .pending(handlers) = self.state {
-            self.state = .resolved(resolution)
+        if case let .pending(handlers) = state {
+            state = .resolved(resolution)
             handlers.objects.forEach { $0(resolution) }
         }
         lock.unlock()
     }
-    
+
+    /// The provided closure executes asynchronously when the promise resolves.
+    ///
+    /// - parameter on: A queue on which the closure is executed. `.main` by default.
     public func completion(on queue: DispatchQueue = .main, _ closure: @escaping (PromiseResolution<T>) -> Void) {
         let completion: (PromiseResolution<T>) -> Void = { resolution in
             queue.async { closure(resolution) }
         }
         lock.lock()
-        switch self.state {
+        switch state {
         case let .pending(handlers): handlers.objects.append(completion)
         case let .resolved(resolution): completion(resolution)
         }
@@ -46,14 +77,32 @@ public final class Promise<T> {
 }
 
 public extension Promise {
+
+    /// The provided closure executes asynchronously when the promise fulfills
+    /// with a value.
+    ///
+    /// - parameter on: A queue on which the closure is executed. `.main` by default.
+    /// - returns: self
     @discardableResult public func then(on queue: DispatchQueue = .main, _ closure: @escaping (T) -> Void) -> Promise {
         return then(on: queue, fulfilment: closure, rejection: nil)
     }
-    
+
+    /// The provided closure executes asynchronously when the promise fulfills
+    /// with a value.
+    ///
+    /// - parameter on: A queue on which the closure is executed. `.main` by default.
+    /// queue by default.
+    /// - returns: A promise fulfilled with a value returns by the closure.
     public func then<U>(on queue: DispatchQueue = .main, _ closure: @escaping (T) -> U) -> Promise<U> {
         return then(on: queue) { Promise<U>(value: closure($0)) }
     }
-    
+
+    /// The provided closure executes asynchronously when the promise fulfills
+    /// with a value.
+    ///
+    /// - parameter on: A queue on which the closure is executed. `.main` by default.
+    /// - returns: A promise that resolves with the resolution of the promise
+    /// returned by the given closure. Allows to chain promises.
     public func then<U>(on queue: DispatchQueue = .main, _ closure: @escaping (T) -> Promise<U>) -> Promise<U> {
         return Promise<U>() { fulfill, reject in
             then(
@@ -66,11 +115,20 @@ public extension Promise {
                 rejection: { reject($0) }) // bubble up error
         }
     }
-    
+
+    /// The provided closure executes asynchronously when the promise is
+    /// rejected with an error.
+    ///
+    /// - parameter on: A queue on which the closure is executed. `.main` by default.
     @discardableResult public func `catch`(on queue: DispatchQueue = .main, _ closure: @escaping (Error) -> Void) {
         then(on: queue, fulfilment: nil, rejection: closure)
     }
-    
+
+    /// The provided closure executes asynchronously when the promise is rejected.
+    /// Unlike `catch` `recover` allows you to continue the chain of promises
+    /// by recovering from an error with a new promise.
+    ///
+    /// - parameter on: A queue on which the closure is executed. `.main` by default.
     public func recover(on queue: DispatchQueue = .main, _ closure: @escaping (Error) -> Promise) -> Promise {
         return Promise() { fulfill, reject in
             then(
@@ -83,7 +141,10 @@ public extension Promise {
             })
         }
     }
-    
+
+    /// The provided closure executes asynchronously when the promise resolves.
+    ///
+    /// - parameter on: A queue on which the closure is executed. `.main` by default.
     @discardableResult public func then(on queue: DispatchQueue = .main, fulfilment: ((T) -> Void)?, rejection: ((Error) -> Void)?) -> Promise {
         completion(on: queue) { resolution in
             switch resolution {
