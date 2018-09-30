f37538954dfdf7fb5462c6c0aa4fd1f7839bef5a
Michael Nisi <michael.nisi@gmail.com>
2018-09-30 07:40:20 +0200

    Reset task ID

--- Sources/ImageView.swift
+++ Sources/ImageView.swift
@@ -321,6 +321,9 @@ private final class ImageViewController {
         if request.memoryCacheOptions.isReadAllowed,
             let imageCache = pipeline.configuration.imageCache,
             let response = imageCache.cachedResponse(for: request) {
+
+            self.taskId = 0
+
             handle(response: response, error: nil, fromMemCache: true, options: options)
             completion?(response, nil)
             return nil
