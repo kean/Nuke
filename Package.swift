ea03f895aea1383dda0cceff3e0497e04dd32221
Vadim Shpakovski <vadim@shpakovski.com>
2019-04-08 08:57:27 +0300

    Swift PM: Include platform for code compatibility

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "Nuke",
+    platforms: [
+        .macOS(.v10_12)
+    ],
     products: [
         .library(name: "Nuke", targets: ["Nuke"]),
     ],
