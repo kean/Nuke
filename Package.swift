75068260d0f829f1beb4e06bc7298fc90047768a
kean <grebenyuk.alexander@gmail.com>
2019-04-13 11:49:04 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,10 @@ import PackageDescription
 let package = Package(
     name: "Nuke",
     platforms: [
-        .macOS(.v10_12)
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS(.v3)
     ],
     products: [
         .library(name: "Nuke", targets: ["Nuke"]),
