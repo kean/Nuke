222201b4ebbfd84349f52ad7b6682ab00a265f39
kean <grebenyuk.alexander@gmail.com>
2019-05-15 21:36:49 +0200

    Remove redundant Nuke.h

--- Supporting Files/Nuke.h
+++
@@ -1,8 +0,0 @@
-// The MIT License (MIT)
-//
-// Copyright (c) 2015-2019 Alexander Grebenyuk (github.com/kean).
-
-@import Foundation;
-
-FOUNDATION_EXPORT double NukeVersionNumber;
-FOUNDATION_EXPORT const unsigned char NukeVersionString[];
