079922fdfc7daab2bf770b1aa27c12e4395a16de
kean <grebenyuk.alexander@gmail.com>
2019-07-21 14:04:29 +0200

    cleaup scripts

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -7,35 +7,38 @@ xcode=$2
 set -o pipefail
 xcodebuild -version
 
+function build() {
+	for destination in "$@"; do
+		xcodebuild build -scheme $scheme -destination "$destination" | xcpretty;
+	done
+}
+
+function test() {
+	xcodebuild build-for-testing -scheme $scheme -destination "$1" | xcpretty;
+
+	for destination in "$@"; do
+		# passing multiple destinations to `test` command results in Travis hanging
+		xcodebuild test-without-building -scheme $scheme -destination "$destination" | xcpretty;
+	done
+}
+
 if [ $platform == "iOS" ]; then
 
 	if [ $xcode == "xcode11" ]; then
-		xcodebuild test -scheme $scheme -destination "OS=13.0,name=iPhone Xs" | xcpretty;
+		test "OS=13.0,name=iPhone Xs"
 	else
-		xcodebuild build-for-testing -scheme $scheme -destination "OS=12.2,name=iPhone X" | xcpretty;
-
-		# passing multiple destinations to `test` command results in Travis hanging
-		xcodebuild test-without-building -scheme $scheme -destination "OS=12.2,name=iPhone X" | xcpretty;
-		xcodebuild test-without-building -scheme $scheme -destination "OS=11.4,name=iPhone X" | xcpretty;
-		xcodebuild test-without-building -scheme $scheme -destination "OS=10.3.1,name=iPhone SE" | xcpretty;
+		test "OS=12.2,name=iPhone X" "OS=11.4,name=iPhone X" "OS=10.3.1,name=iPhone SE"
 	fi
 fi
 
 if [ $platform == "macOS" ]; then
-	xcodebuild test -scheme $scheme -destination "arch=x86_64" | xcpretty;
+	test "arch=x86_64"
 fi
 
 if [ $platform == "watchOS" ]; then
-	xcodebuild build -scheme $scheme -destination "OS=4.2,name=Apple Watch - 42mm" | xcpretty;
-	xcodebuild build -scheme $scheme -destination "OS=3.2,name=Apple Watch - 42mm" | xcpretty;
+	build "OS=4.2,name=Apple Watch - 42mm" "OS=3.2,name=Apple Watch - 42mm"
 fi
 
 if [ $platform == "tvOS" ]; then
-	xcodebuild build-for-testing -scheme $scheme -destination "OS=12.2,name=Apple TV 4K" | xcpretty;
-
-	# passing multiple destinations to `test` command results in Travis hanging
-	xcodebuild test-without-building -scheme $scheme -destination "OS=12.2,name=Apple TV 4K" | xcpretty;
-	xcodebuild test-without-building -scheme $scheme -destination "OS=11.3,name=Apple TV 4K" | xcpretty;
+	test "OS=12.2,name=Apple TV 4K" "OS=11.3,name=Apple TV 4K"
 fi
-
-exit $?
