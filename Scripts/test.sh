f4c21fd650365e9b4f610278dd07076e6ffa9807
kean <grebenyuk.alexander@gmail.com>
2019-07-21 18:15:48 +0200

    Run Thread Safety tests on CI

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -2,14 +2,16 @@
 
 scheme="Nuke"
 
-while getopts "d:" opt; do
+while getopts "s:d:" opt; do
     case $opt in
+    	s) scheme=${OPTARG};;
         d) destinations+=("$OPTARG");;
         #...
     esac
 done
 shift $((OPTIND -1))
 
+echo "scheme = ${scheme}"
 echo "destinations = ${destinations[@]}"
 
 
@@ -17,11 +19,11 @@ set -o pipefail
 xcodebuild -version
 
 
-xcodebuild build-for-testing -scheme $scheme -destination "${destinations[0]}" | xcpretty;
+xcodebuild build-for-testing -scheme "$scheme" -destination "${destinations[0]}" | xcpretty;
 
 for destination in "${destinations[@]}"; do
 	echo "\nRunning tests for destination: $destination"
 
 	# passing multiple destinations to `test` command results in Travis hanging
-	xcodebuild test-without-building -scheme $scheme -destination "$destination" | xcpretty;
+	xcodebuild test-without-building -scheme "$scheme" -destination "$destination" | xcpretty;
 done
