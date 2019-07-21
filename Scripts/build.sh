f74d1501bc3e444155d253aff457f257969b46b9
kean <grebenyuk.alexander@gmail.com>
2019-07-21 17:50:07 +0200

    Rework travis setup

---
+++ Scripts/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+scheme="Nuke"
+
+while getopts "d:" opt; do
+    case $opt in
+        d) destinations+=("$OPTARG");;
+        #...
+    esac
+done
+shift $((OPTIND -1))
+
+echo "destinations = ${destinations[@]}"
+
+set -o pipefail
+xcodebuild -version
+
+for dest in "${destinations[@]}"; do
+	echo "Building for destination: $dest"
+	xcodebuild build -scheme $scheme -destination "$dest" | xcpretty;
+done
