f7b7a0162932f71dfcf0523c749f80f082b67627
Abdallah Nehme <anehme@totersapp.com>
2019-10-28 11:59:45 +0200

    add some comments

--- Sources/SpeedManager.swift
+++ Sources/SpeedManager.swift
@@ -7,17 +7,19 @@
 //
 
 import Foundation
+/// Provides Last downloaded speed.
 public final class SpeedManager{
     var startTime: CFAbsoluteTime!
     var stopTime: CFAbsoluteTime!
     var bytesReceived: Int!
-    public static var downloadSpeed = 100.0
+    public static var downloadSpeed = 100.0 // start with heighest speed
     init(){
         startTime = CFAbsoluteTimeGetCurrent()
         stopTime = startTime
         bytesReceived = 0
     }
-
+    /// - parameter data: Downloaded data
+    /// - parameter didCompleteWithError: Catch error in downloading data
     func update(with data: Data?, didCompleteWithError error: Error? ){
         if let data = data{
             bytesReceived! += data.count
