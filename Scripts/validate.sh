3ce3fa5e09933193e1d29548120414d47996968f
kean <grebenyuk.alexander@gmail.com>
2019-11-12 07:38:38 -0500

    Separate linter phase in install and script parts

--- Scripts/validate.sh
+++ Scripts/validate.sh
@@ -1,14 +1,3 @@
 #!/bin/sh
 
-# Install SwiftLint
-
-# -L to enable redirects
-curl -L 'https://github.com/realm/SwiftLint/releases/download/0.36.0/portable_swiftlint.zip' -o swiftlint.zip
-mkdir temp
-unzip swiftlint.zip -d temp
-rm -f swiftlint.zip
-
-# Perform the actual validation
-
 ./temp/swiftlint lint --strict
-rm -rf temp
