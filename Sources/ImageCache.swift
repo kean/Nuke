f0b1e6f832f9fb0869bbcbb34c0dcbff069b71fb
kean <grebenyuk.alexander@gmail.com>
2020-03-28 13:01:46 -0400

    ImageCache to use ImageContainer.data when calculating cost

--- Sources/ImageCache.swift
+++ Sources/ImageCache.swift
@@ -110,7 +110,7 @@ public final class ImageCache: ImageCaching {
 
     /// Stores the given `ImageResponse` in the cache using the given request.
     public func storeResponse(_ response: ImageResponse, for request: ImageRequest) {
-        impl.set(response, forKey: request.makeCacheKeyForFinalImage(), cost: self.cost(for: response.image))
+        impl.set(response, forKey: request.makeCacheKeyForFinalImage(), cost: self.cost(for: response.container))
     }
 
     /// Removes response stored with the given request.
@@ -135,18 +135,18 @@ public final class ImageCache: ImageCaching {
     }
 
     /// Returns cost for the given image by approximating its bitmap size in bytes in memory.
-    func cost(for image: PlatformImage) -> Int {
+    func cost(for container: ImageContainer) -> Int {
         let dataCost: Int
         if ImagePipeline.Configuration.isAnimatedImageDataEnabled {
-            dataCost = image.animatedImageData?.count ?? 0
+            dataCost = container.image.animatedImageData?.count ?? 0
         } else {
-            dataCost = 0
+            dataCost = container.data?.count ?? 0
         }
 
         // bytesPerRow * height gives a rough estimation of how much memory
         // image uses in bytes. In practice this algorithm combined with a
         // concervative default cost limit works OK.
-        guard let cgImage = image.cgImage else {
+        guard let cgImage = container.image.cgImage else {
             return 1 + dataCost
         }
         return cgImage.bytesPerRow * cgImage.height + dataCost
