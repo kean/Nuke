74d233c263816e7e642bf82dd06e698c0ad710e3
kean <grebenyuk.alexander@gmail.com>
2020-04-01 17:02:38 -0400

    Update SwiftLint to 0.39.1

--- Scripts/install_swiftlint.sh
+++ Scripts/install_swiftlint.sh
@@ -2,7 +2,7 @@
 
 # -L to enable redirects
 echo "Installing SwiftLint by downloading a pre-compiled binary"
-curl -L 'https://github.com/realm/SwiftLint/releases/download/0.37.0/portable_swiftlint.zip' -o swiftlint.zip
+curl -L 'https://github.com/realm/SwiftLint/releases/download/0.39.1/portable_swiftlint.zip' -o swiftlint.zip
 mkdir temp
 unzip swiftlint.zip -d temp
 rm -f swiftlint.zip
