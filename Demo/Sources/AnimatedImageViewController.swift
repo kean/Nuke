5890984f497956e9cfd1a6b5821e9aac8d956336
kean <grebenyuk.alexander@gmail.com>
2020-11-26 23:28:53 -0500

    Remove broken links from GIF demo

--- Demo/Sources/AnimatedImageViewController.swift
+++ Demo/Sources/AnimatedImageViewController.swift
@@ -17,10 +17,21 @@ final class AnimatedImageViewController: UICollectionViewController, UICollectio
         super.init(collectionViewLayout: UICollectionViewFlowLayout())
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        ImagePipeline.Configuration.isAnimatedImageDataEnabled = true
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        ImagePipeline.Configuration.isAnimatedImageDataEnabled = false
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        collectionView?.register(HeaderCell.self, forCellWithReuseIdentifier: headerCellReuseId)
         collectionView?.register(AnimatedImageCell.self, forCellWithReuseIdentifier: imageCellReuseID)
 
         if #available(iOS 13.0, *) {
@@ -48,29 +59,18 @@ final class AnimatedImageViewController: UICollectionViewController, UICollectio
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        switch indexPath.section {
-        case 0:
-            return collectionView.dequeueReusableCell(withReuseIdentifier: headerCellReuseId, for: indexPath)
-        default:
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: imageCellReuseID, for: indexPath) as! AnimatedImageCell
-            cell.setImage(with: imageURLs[indexPath.row])
-            return cell
-        }
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: imageCellReuseID, for: indexPath) as! AnimatedImageCell
+        cell.setImage(with: imageURLs[indexPath.row])
+        return cell
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let layout = collectionViewLayout as! UICollectionViewFlowLayout
         let width = view.bounds.size.width - layout.sectionInset.left - layout.sectionInset.right
-        switch indexPath.section {
-        case 0:
-            return CGSize(width: width, height: 50)
-        default:
-            return CGSize(width: width, height: width)
-        }
+        return CGSize(width: width, height: width)
     }
 }
 
-private let headerCellReuseId = "headerCellReuseID"
 private let imageCellReuseID = "imageCellReuseID"
 
 // MARK: - Image URLs
@@ -153,34 +153,5 @@ private final class AnimatedImageCell: UICollectionViewCell {
         task?.cancel()
         spinner.stopAnimating()
         imageView.prepareForReuse()
-    }
-}
-
-// MARK: - HeaderCell
-
-private final class HeaderCell: UICollectionViewCell {
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-
-        let textView = UITextView()
-        if #available(iOS 13.0, *) {
-            textView.textColor = UIColor.label
-        } else {
-            textView.textColor = UIColor.black
-        }
-        textView.font = UIFont.systemFont(ofSize: 16)
-        textView.isEditable = false
-        textView.textAlignment = .center
-        textView.dataDetectorTypes = .link
-
-        contentView.addSubview(textView)
-        textView.frame = contentView.bounds
-        textView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-
-        textView.text = "Images by Florian de Looij\n http://flrngif.tumblr.com"
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError()
     }
 }
