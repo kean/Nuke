3f9c11361004e773d63759d7455658ffee627e33
Ilya Laryionau <larryonoff@gmail.com>
2021-09-02 17:54:54 +0300

    Migrate video decoding from NukeUI

---
+++ Sources/Core/Video/AVAsset.swift
@@ -0,0 +1,75 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2015-2021 Alexander Grebenyuk (github.com/kean).
+
+import AVKit
+import Foundation
+
+#if !os(watchOS)
+
+extension ImageContainer {
+    /// A fetched video from data.
+    public var asset: AVAsset? {
+        guard type == .mp4, let data = data else {
+            return nil
+        }
+        return AVDataAsset(data: data)
+    }
+}
+
+// This allows LazyImage to play video from memory.
+final class AVDataAsset: AVURLAsset {
+    private let _loader: DataAssetResourceLoader
+
+    init(data: Data) {
+        self._loader = DataAssetResourceLoader(
+            data: data,
+            contentType: AVFileType.mp4.rawValue
+        )
+
+        // The URL is irrelevant
+        let url = URL(string: "in-memory-data://\(UUID().uuidString)") ?? URL(fileURLWithPath: "/dev/null")
+        super.init(url: url, options: nil)
+
+        resourceLoader.setDelegate(_loader, queue: .global())
+    }
+}
+
+// This allows LazyImage to play video from memory.
+private final class DataAssetResourceLoader: NSObject, AVAssetResourceLoaderDelegate {
+    private let data: Data
+    private let contentType: String
+
+    init(data: Data, contentType: String) {
+        self.data = data
+        self.contentType = contentType
+    }
+
+    // MARK: - DataAssetResourceLoader
+
+    func resourceLoader(
+        _ resourceLoader: AVAssetResourceLoader,
+        shouldWaitForLoadingOfRequestedResource loadingRequest: AVAssetResourceLoadingRequest
+    ) -> Bool {
+        if let contentRequest = loadingRequest.contentInformationRequest {
+            contentRequest.contentType = contentType
+            contentRequest.contentLength = Int64(data.count)
+            contentRequest.isByteRangeAccessSupported = true
+        }
+
+        if let dataRequest = loadingRequest.dataRequest {
+            if dataRequest.requestsAllDataToEndOfResource {
+                dataRequest.respond(with: data[dataRequest.requestedOffset...])
+            } else {
+                let range = dataRequest.requestedOffset..<(dataRequest.requestedOffset + Int64(dataRequest.requestedLength))
+                dataRequest.respond(with: data[range])
+            }
+        }
+
+        loadingRequest.finishLoading()
+
+        return true
+    }
+}
+
+#endif
---
+++ Sources/Core/Video/VideoDecoding.swift
@@ -0,0 +1,85 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2015-2021 Alexander Grebenyuk (github.com/kean).
+
+
+#if !os(watchOS)
+
+import Foundation
+import AVKit
+
+extension ImageType {
+    public static let mp4: ImageType = "public.mp4"
+}
+
+extension ImageDecoders {
+    public final class Video: ImageDecoding, ImageDecoderRegistering {
+        private var didProducePreview = false
+
+        static func isVideo(_ data: Data) -> Bool {
+            match(data, offset: 4, [0x66, 0x74, 0x79, 0x70])
+        }
+
+        public var isAsynchronous: Bool {
+            false
+        }
+
+        public init?(data: Data, context: ImageDecodingContext) {
+            guard Video.isVideo(data) else { return nil }
+        }
+
+        public init?(partiallyDownloadedData data: Data, context: ImageDecodingContext) {
+            guard Video.isVideo(data) else { return nil }
+        }
+
+        public func decode(_ data: Data) -> ImageContainer? {
+            ImageContainer(image: PlatformImage(), type: .mp4, data: data)
+        }
+
+        public func decodePartiallyDownloadedData(_ data: Data) -> ImageContainer? {
+            guard !didProducePreview else {
+                return nil // We only need one preview
+            }
+            guard let preview = makePreview(for: data) else {
+                return nil
+            }
+            didProducePreview = true
+            return ImageContainer(image: preview, type: .mp4, isPreview: true, data: data)
+        }
+
+        private static var isRegistered: Bool = false
+
+        public static func register() {
+            guard !isRegistered else { return }
+            isRegistered = true
+
+            ImageDecoderRegistry.shared.register(ImageDecoders.Video.self)
+        }
+    }
+}
+
+private func makePreview(for data: Data) -> PlatformImage? {
+    let asset = AVDataAsset(data: data)
+    let generator = AVAssetImageGenerator(asset: asset)
+    guard let cgImage = try? generator.copyCGImage(at: CMTime(value: 0, timescale: 1), actualTime: nil) else {
+        return nil
+    }
+    #if os(macOS)
+    return PlatformImage(cgImage: cgImage, size: .zero)
+    #else
+    return PlatformImage(cgImage: cgImage)
+    #endif
+}
+
+// TODO: extened support for other image formats
+// ftypisom - ISO Base Media file (MPEG-4) v1
+// There are a bunch of other ways to create MP4
+// https://www.garykessler.net/library/file_sigs.html
+private func match(_ data: Data, offset: Int = 0, _ numbers: [UInt8]) -> Bool {
+    guard data.count >= numbers.count + offset else { return false }
+    return !zip(numbers.indices, numbers).contains { (index, number) in
+        data[index + offset] != number
+    }
+}
+
+#endif
