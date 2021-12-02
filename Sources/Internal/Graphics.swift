6b8c0683aae35dc2b3c3ab71b8337ba4a64ef0cd
kean <grebenyuk.alexander@gmail.com>
2021-12-02 07:29:42 -0500

    Revert prepareForDisplay changes

--- Sources/Internal/Graphics.swift
+++ Sources/Internal/Graphics.swift
@@ -146,12 +146,6 @@ extension PlatformImage {
 
     /// Decompresses the input image by drawing in the the `CGContext`.
     func decompressed() -> PlatformImage? {
-        #if swift(>=5.5) && (os(iOS) || os(tvOS)) && !targetEnvironment(macCatalyst)
-        if #available(iOS 15.0, tvOS 15.0, *) {
-            return preparingForDisplay()
-        }
-        #endif
-
         guard let cgImage = cgImage else {
             return nil
         }
