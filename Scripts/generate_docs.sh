d6bd9f884e1f43ec60048b6b0b20cfaaf055f4c8
kean <grebenyuk.alexander@gmail.com>
2022-05-21 12:23:52 -0400

    Update generate_docs script

--- Scripts/generate_docs.sh
+++ Scripts/generate_docs.sh
@@ -2,7 +2,7 @@
 
 version=$1
 
-swift doc generate ./Sources \
+swift-doc generate ./Sources \
     --module-name Nuke \
     --format html \
     --base-url "https://kean-org.github.io/docs/nuke/reference/$version"
