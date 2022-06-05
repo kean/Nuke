659e84e7954ed43e90313b4fb6103ff54b8a601f
kean <grebenyuk.alexander@gmail.com>
2022-06-05 11:21:40 -0400

    Remove ImageType

--- Sources/Internal/Deprecated.swift
+++ Sources/Internal/Deprecated.swift
@@ -3,7 +3,3 @@
 // Copyright (c) 2015-2022 Alexander Grebenyuk (github.com/kean).
 
 import Foundation
-
-// Deprecated in 10.5.0
-@available(*, deprecated, message: "Please use AssetType instead")
-public typealias ImageType = AssetType
