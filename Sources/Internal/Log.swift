aca7938e2c301bdb19512f783319002d0e04c68c
kean <grebenyuk.alexander@gmail.com>
2022-06-10 14:41:35 -0400

    Remove unused signpost variant

--- Sources/Internal/Log.swift
+++ Sources/Internal/Log.swift
@@ -19,22 +19,15 @@ func signpost(_ log: OSLog, _ object: AnyObject, _ name: StaticString, _ type: O
     os_signpost(type, log: log, name: name, signpostID: signpostId, "%{public}s", message())
 }
 
-func signpost<T>(_ log: OSLog, _ name: StaticString, _ work: () throws -> T) rethrows -> T {
+func signpost<T>(_ log: OSLog, _ name: StaticString, _ message: @autoclosure () -> String? = nil, _ work: () throws -> T) rethrows -> T {
     guard ImagePipeline.Configuration.isSignpostLoggingEnabled else { return try work() }
 
     let signpostId = OSSignpostID(log: log)
-    os_signpost(.begin, log: log, name: name, signpostID: signpostId)
-    let result = try work()
-    os_signpost(.end, log: log, name: name, signpostID: signpostId)
-    return result
-}
-
-// TODO: Do we need this version?
-func signpost<T>(_ log: OSLog, _ name: StaticString, _ message: @autoclosure () -> String, _ work: () throws -> T) rethrows -> T {
-    guard ImagePipeline.Configuration.isSignpostLoggingEnabled else { return try work() }
-
-    let signpostId = OSSignpostID(log: log)
-    os_signpost(.begin, log: log, name: name, signpostID: signpostId, "%{public}s", message())
+    if let message = message() {
+        os_signpost(.begin, log: log, name: name, signpostID: signpostId, "%{public}s", message)
+    } else {
+        os_signpost(.begin, log: log, name: name, signpostID: signpostId)
+    }
     let result = try work()
     os_signpost(.end, log: log, name: name, signpostID: signpostId)
     return result
