4adf5542551f89293e0941af03d7629a8c539e40
kean <grebenyuk.alexander@gmail.com>
2022-06-12 18:49:20 -0400

    Move LazyImageState to a separate file

---
+++ Sources/NukeUI/LazyImgeState.swift
@@ -0,0 +1,63 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2015-2021 Alexander Grebenyuk (github.com/kean).
+
+import Foundation
+import Nuke
+import SwiftUI
+import Combine
+
+public struct LazyImageState {
+    /// Returns the current fetch result.
+    public let result: Result<ImageResponse, Error>?
+
+    /// Returns a current error.
+    public var error: Error? {
+        if case .failure(let error) = result {
+            return error
+        }
+        return nil
+    }
+
+    /// Returns an image view.
+    @MainActor
+    public var image: Image? {
+#if os(macOS)
+        return imageContainer.map { Image($0) }
+#elseif os(watchOS)
+        return imageContainer.map { Image(uiImage: $0.image) }
+#else
+        return imageContainer.map { Image($0) }
+#endif
+    }
+
+    /// Returns the fetched image.
+    ///
+    /// - note: In case pipeline has `isProgressiveDecodingEnabled` option enabled
+    /// and the image being downloaded supports progressive decoding, the `image`
+    /// might be updated multiple times during the download.
+    public let imageContainer: ImageContainer?
+
+    /// Returns `true` if the image is being loaded.
+    public let isLoading: Bool
+
+    /// The download progress.
+    public struct Progress: Equatable {
+        /// The number of bytes that the task has received.
+        public let completed: Int64
+
+        /// A best-guess upper bound on the number of bytes the client expects to send.
+        public let total: Int64
+    }
+
+    /// The progress of the image download.
+    public let progress: Progress
+
+    @MainActor
+    init(_ fetchImage: FetchImage) {
+        self.result = fetchImage.result
+        self.imageContainer = fetchImage.imageContainer
+        self.isLoading = fetchImage.isLoading
+        self.progress = Progress(completed: fetchImage.progress.completed, total: fetchImage.progress.total)
+    }
+}
