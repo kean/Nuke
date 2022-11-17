e82894acdad7fd59ebe21d3404e817c3c809523d
Masoud Sh <zzmasoud@gmail.com>
2022-11-17 10:42:25 +0100

    Add tests for `ImageProcessors.Resize` protocol extensions default methods

--- Tests/NukeTests/ImageProcessorsTests/ImageProcessorsProtocolExtensionsTests.swift
+++ Tests/NukeTests/ImageProcessorsTests/ImageProcessorsProtocolExtensionsTests.swift
@@ -6,9 +6,29 @@ import XCTest
 import Nuke
 
 class ImageProcessorsProtocolExtensionsTests: XCTestCase {
+    
+    func testPassingProcessorsUsingProtocolExtensionsResize() throws {
+        let size = CGSize(width: 100, height: 100)
+        let processor = ImageProcessors.Resize(size: size)
+        
+        let request = try XCTUnwrap(ImageRequest(url: nil, processors: [.resize(size: size)]))
+        
+        XCTAssertEqual(request.processors.first?.identifier, processor.identifier)
+    }
 
-    func testPassingProcessorsUsingProtocolExtensions() throws {
-        // Just make sure it compiles
-        _ = ImageRequest(url: nil, processors: [.resize(width: 100)])
+    func testPassingProcessorsUsingProtocolExtensionsResizeWidthOnly() throws {
+        let processor = ImageProcessors.Resize(width: 100)
+        
+        let request = try XCTUnwrap(ImageRequest(url: nil, processors: [.resize(width: 100)]))
+        
+        XCTAssertEqual(request.processors.first?.identifier, processor.identifier)
+    }
+    
+    func testPassingProcessorsUsingProtocolExtensionsHeightOnly() throws {
+        let processor = ImageProcessors.Resize(height: 100)
+        
+        let request = try XCTUnwrap(ImageRequest(url: nil, processors: [.resize(height: 100)]))
+        
+        XCTAssertEqual(request.processors.first?.identifier, processor.identifier)
     }
 }
