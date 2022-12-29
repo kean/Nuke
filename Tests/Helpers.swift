69032a74fc6b92968420c46b81cdd323a46f3ac1
kean <grebenyuk.alexander@gmail.com>
2022-12-28 19:37:36 -0500

    Fix tsan warning in tests

--- Tests/Helpers.swift
+++ Tests/Helpers.swift
@@ -170,3 +170,36 @@ extension Result {
         }
     }
 }
+
+@propertyWrapper final class Atomic<T> {
+    private var value: T
+    private let lock: os_unfair_lock_t
+
+    init(wrappedValue value: T) {
+        self.value = value
+        self.lock = .allocate(capacity: 1)
+        self.lock.initialize(to: os_unfair_lock())
+    }
+
+    deinit {
+        lock.deinitialize(count: 1)
+        lock.deallocate()
+    }
+
+    var wrappedValue: T {
+        get { getValue() }
+        set { setValue(newValue) }
+    }
+
+     private func getValue() -> T {
+        os_unfair_lock_lock(lock)
+        defer { os_unfair_lock_unlock(lock) }
+        return value
+    }
+
+    private func setValue(_ newValue: T) {
+        os_unfair_lock_lock(lock)
+        defer { os_unfair_lock_unlock(lock) }
+        value = newValue
+    }
+}
