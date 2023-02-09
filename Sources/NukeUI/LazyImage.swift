a331f4aa0e08765c42fb8689e2ff3aa9be3c08cf
kean <grebenyuk.alexander@gmail.com>
2023-02-09 18:44:33 -0500

    Pass default scale 1

--- Sources/NukeUI/LazyImage.swift
+++ Sources/NukeUI/LazyImage.swift
@@ -39,8 +39,8 @@ public struct LazyImage<Content: View>: View {
     ///
     /// - Parameters:
     ///   - url: The image URL.
-    public init(url: URL?) where Content == Image {
-        self.init(request: url.map { ImageRequest(url: $0) })
+    public init(url: URL?, scale: CGFloat = 1) where Content == Image {
+        self.init(request: url.map { ImageRequest(url: $0, userInfo: [.scaleKey: scale]) })
     }
 
     /// Loads and displays an image using ``Image``.
