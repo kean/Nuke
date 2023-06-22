0655d2b3aa7abb7a80db6a08d7db68773456423b
kyrylokhlopko <khlopko@gmail.com>
2023-06-22 17:19:11 +0300

    #693: DataPublisher created from async function should start lazily

---
+++ Tests/NukeTests/DataPublisherTests.swift
@@ -0,0 +1,38 @@
+//
+//  DataPublisherTests.swift
+//
+
+import XCTest
+import Combine
+@testable import Nuke
+
+internal final class DataPublisherTests: XCTestCase {
+
+    private var cancellable: (any Nuke.Cancellable)?
+
+    func testInitNotStartsExecutionRightAway() {
+        let operation = MockOperation()
+        let publisher = DataPublisher(id: UUID().uuidString, { await operation.execute() })
+
+        XCTAssertEqual(0, operation.executeCalls)
+
+        let expOp = expectation(description: "Waits for MockOperation to complete execution")
+        cancellable = publisher.sink { completion in expOp.fulfill() } receiveValue: { _ in }
+        waitForExpectations(timeout: 0.1)
+
+        XCTAssertEqual(1, operation.executeCalls)
+    }
+
+    private final class MockOperation: @unchecked Sendable {
+
+        private(set) var executeCalls = 0
+
+        func execute() async -> Data {
+            executeCalls += 1
+            await Task.yield()
+            return Data()
+        }
+
+    }
+
+}
