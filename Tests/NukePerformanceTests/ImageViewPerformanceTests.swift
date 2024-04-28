4a7f2b1f7ec4ed7441c4f64592f0494f61bbc10e
kean <grebenyuk.alexander@gmail.com>
2024-04-27 21:01:18 -0400

    Fix warning in ImageViewPerformanceTests on Swift 6

--- Tests/NukePerformanceTests/ImageViewPerformanceTests.swift
+++ Tests/NukePerformanceTests/ImageViewPerformanceTests.swift
@@ -6,7 +6,6 @@ import XCTest
 import Nuke
 import NukeExtensions
 
-@MainActor
 class ImageViewPerformanceTests: XCTestCase {
     private let dummyCacheRequest = ImageRequest(url: URL(string: "http://test.com/9999999)")!, processors: [ImageProcessors.Resize(size: CGSize(width: 2, height: 2))])
 
