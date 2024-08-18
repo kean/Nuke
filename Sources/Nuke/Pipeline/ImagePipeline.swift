7e34b50c640e0a650ea16c81ac872d1583b09ff1
kean <grebenyuk.alexander@gmail.com>
2024-08-18 13:49:36 -0400

    Soft-deprecate closure-based APIs

--- Sources/Nuke/Pipeline/ImagePipeline.swift
+++ Sources/Nuke/Pipeline/ImagePipeline.swift
@@ -101,7 +101,7 @@ public final class ImagePipeline {
         }
     }
 
-    // MARK: - Loading Images (Async/Await)
+    // MARK: - Loading Images
 
     /// Creates a task with the given URL.
     ///
@@ -127,7 +127,7 @@ public final class ImagePipeline {
         try await imageTask(with: request).image
     }
 
-    // MARK: - Loading Data (Async/Await)
+    // MARK: - Loading Data
 
     /// Returns image data for the given request.
     ///
@@ -136,102 +136,6 @@ public final class ImagePipeline {
         let task = makeImageTask(with: request, isDataTask: true)
         let response = try await task.response
         return (response.container.data ?? Data(), response.urlResponse)
-    }
-
-    // MARK: - Loading Images (Closures)
-
-    /// Loads an image for the given request.
-    ///
-    /// - parameters:
-    ///   - request: An image request.
-    ///   - completion: A closure to be called on the main thread when the request
-    ///   is finished.
-    @discardableResult public nonisolated func loadImage(
-        with url: URL,
-        completion: @MainActor @Sendable @escaping (_ result: Result<ImageResponse, Error>) -> Void
-    ) -> ImageTask {
-        _loadImage(with: ImageRequest(url: url), progress: nil, completion: completion)
-    }
-
-    /// Loads an image for the given request.
-    ///
-    /// - parameters:
-    ///   - request: An image request.
-    ///   - completion: A closure to be called on the main thread when the request
-    ///   is finished.
-    @discardableResult public nonisolated func loadImage(
-        with request: ImageRequest,
-        completion: @MainActor @Sendable @escaping (_ result: Result<ImageResponse, Error>) -> Void
-    ) -> ImageTask {
-        _loadImage(with: request, progress: nil, completion: completion)
-    }
-
-    /// Loads an image for the given request.
-    ///
-    /// - parameters:
-    ///   - request: An image request.
-    ///   - progress: A closure to be called periodically on the main thread when
-    ///   the progress is updated.
-    ///   - completion: A closure to be called on the main thread when the request
-    ///   is finished.
-    @discardableResult public nonisolated func loadImage(
-        with request: ImageRequest,
-        progress: (@MainActor @Sendable (_ response: ImageResponse?, _ completed: Int64, _ total: Int64) -> Void)?,
-        completion: @MainActor @Sendable @escaping (_ result: Result<ImageResponse, Error>) -> Void
-    ) -> ImageTask {
-        _loadImage(with: request, progress: {
-            progress?($0, $1.completed, $1.total)
-        }, completion: completion)
-    }
-
-    private nonisolated func _loadImage(
-        with request: ImageRequest,
-        isDataTask: Bool = false,
-        progress: (@MainActor @Sendable (ImageResponse?, ImageTask.Progress) -> Void)?,
-        completion: @MainActor @Sendable @escaping (Result<ImageResponse, Error>) -> Void
-    ) -> ImageTask {
-        makeImageTask(with: request, isDataTask: isDataTask) { event, task in
-            DispatchQueue.main.async {
-                // The callback-based API guarantees that after cancellation no
-                // event are called on the callback queue.
-                guard !task.isCancelling else { return }
-                switch event {
-                case .progress(let value): progress?(nil, value)
-                case .preview(let response): progress?(response, task.currentProgress)
-                case .cancelled: break // The legacy APIs do not send cancellation events
-                case .finished(let result): completion(result)
-                }
-            }
-        }
-    }
-
-    // MARK: - Loading Data (Closures)
-
-    /// Loads the image data for the given request. The data doesn't get decoded
-    /// or processed in any other way.
-    ///
-    /// You can call ``loadImage(with:completion:)-43osv`` for the request at any point after calling
-    /// ``loadData(with:completion:)-6cwk3``, the pipeline will use the same operation to load the data,
-    /// no duplicated work will be performed.
-    ///
-    /// - parameters:
-    ///   - request: An image request.
-    ///   - progress: A closure to be called periodically on the main thread when the progress is updated.
-    ///   - completion: A closure to be called on the main thread when the request is finished.
-    @discardableResult public nonisolated func loadData(
-        with request: ImageRequest,
-        progress progressHandler: (@MainActor @Sendable (_ completed: Int64, _ total: Int64) -> Void)? = nil,
-        completion: @MainActor @Sendable @escaping (Result<(data: Data, response: URLResponse?), Error>) -> Void
-    ) -> ImageTask {
-        _loadImage(with: request, isDataTask: true) { _, progress in
-            progressHandler?(progress.completed, progress.total)
-        } completion: { result in
-            let result = result.map { response in
-                // Data should never be empty
-                (data: response.container.data ?? Data(), response: response.urlResponse)
-            }
-            completion(result)
-        }
     }
 
     // MARK: - ImageTask (Internal)
