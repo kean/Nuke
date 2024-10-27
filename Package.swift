a4413f67ddc8c96948d88463ce21dce0ce80da04
kean <grebenyuk.alexander@gmail.com>
2024-10-27 12:15:47 -0400

    Bump macOS deployment target to macOS 11

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.9
+// swift-tools-version:6.0
 import PackageDescription
 
 let package = Package(
@@ -6,7 +6,7 @@ let package = Package(
     platforms: [
         .iOS(.v13),
         .tvOS(.v13),
-        .macOS(.v10_15),
+        .macOS(.v11),
         .watchOS(.v6),
         .visionOS(.v1),
     ],
