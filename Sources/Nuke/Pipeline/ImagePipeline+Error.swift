4878846229bb9d900c1e0e80678f1897669d362a
kean <grebenyuk.alexander@gmail.com>
2024-10-27 12:23:37 -0400

    Remove @unchecked from ImagePipeline.Error Sendable conformance

--- Sources/Nuke/Pipeline/ImagePipeline+Error.swift
+++ Sources/Nuke/Pipeline/ImagePipeline+Error.swift
@@ -6,7 +6,7 @@ import Foundation
 
 extension ImagePipeline {
     /// Represents all possible image pipeline errors.
-    public enum Error: Swift.Error, CustomStringConvertible, @unchecked Sendable {
+    public enum Error: Swift.Error, CustomStringConvertible, Sendable {
         /// Returned if data not cached and ``ImageRequest/Options-swift.struct/returnCacheDataDontLoad`` option is specified.
         case dataMissingInCache
         /// Data loader failed to load image data with a wrapped error.
