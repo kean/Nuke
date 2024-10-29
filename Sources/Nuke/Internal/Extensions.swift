fe620ef9694050c7089954654bf8d8aced22ac88
Jacob Myers <myersjac@umich.edu>
2024-10-29 16:07:11 -0400

    fixing comment about .sha256 function should result in

--- Sources/Nuke/Internal/Extensions.swift
+++ Sources/Nuke/Internal/Extensions.swift
@@ -9,7 +9,7 @@ extension String {
     /// Calculates SHA256 from the given string and returns its hex representation.
     ///
     /// ```swift
-    /// print("http://test.com".256)
+    /// print("http://test.com".sha256)
     /// // prints "8b408a0c7163fdfff06ced3e80d7d2b3acd9db900905c4783c28295b8c996165"
     /// ```
     var sha256: String? {
