a38a7872bbc90ab1b392bbf24ff4f4f2df0204a6
Andre Navarro <andre.navarro@gmail.com>
2024-11-08 00:16:19 +0100

    Mark DataLoading completion closures as Sendable

--- Sources/Nuke/Loading/DataLoading.swift
+++ Sources/Nuke/Loading/DataLoading.swift
@@ -11,8 +11,8 @@ public protocol DataLoading: Sendable {
     /// - parameter completion: Must be called once after all (or none in case
     /// of an error) `didReceiveData` closures have been called.
     func loadData(with request: URLRequest,
-                  didReceiveData: @escaping (Data, URLResponse) -> Void,
-                  completion: @escaping (Error?) -> Void) -> any Cancellable
+                  didReceiveData: @Sendable @escaping (Data, URLResponse) -> Void,
+                  completion: @Sendable @escaping (Error?) -> Void) -> any Cancellable
 }
 
 /// A unit of work that can be cancelled.
