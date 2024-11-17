bec2101b77c2c4e4cab6f129d79cf045a3753dfa
kean <grebenyuk.alexander@gmail.com>
2024-11-17 09:20:44 -0500

    Update RateLimiterTests to swift-testing

--- Tests/NukeTests/RateLimiterTests.swift
+++ Tests/NukeTests/RateLimiterTests.swift
@@ -2,74 +2,46 @@
 //
 // Copyright (c) 2015-2024 Alexander Grebenyuk (github.com/kean).
 
-import XCTest
 @testable import Nuke
+import Testing
 
-class RateLimiterTests: XCTestCase {
-    var rateLimiter: RateLimiter!
+@Suite @ImagePipelineActor struct RateLimiterTests {
+    let rateLimiter = RateLimiter(rate: 10, burst: 2)
 
-    override func setUp() {
-        super.setUp()
-
-        // Note: we set very short rate to avoid bucket form being refilled too quickly
-        rateLimiter = RateLimiter(rate: 10, burst: 2)
-    }
-
-    @ImagePipelineActor
-    func testThatBurstIsExecutedimmediately() {
-        // Given
+    @Test func burstIsExecutedImmediately() throws {
         var isExecuted = Array(repeating: false, count: 4)
-
-        // When
         for i in isExecuted.indices {
             rateLimiter.execute {
                 isExecuted[i] = true
                 return true
             }
         }
-
-        // Then
-        XCTAssertEqual(isExecuted, [true, true, false, false], "Expect first 2 items to be executed immediately")
+        try #require(isExecuted == [true, true, false, false], "Expect first 2 items to be executed immediately")
     }
 
-    @ImagePipelineActor
-    func testThatNotExecutedItemDoesntExtractFromBucket() {
-        // Given
+    @Test func NotExecutedItemDoesNotExtractFromBucket() throws {
         var isExecuted = Array(repeating: false, count: 4)
-
-        // When
         for i in isExecuted.indices {
             rateLimiter.execute {
                 isExecuted[i] = true
                 return i != 1 // important!
             }
         }
-
-        // Then
-        XCTAssertEqual(isExecuted, [true, true, true, false], "Expect first 2 items to be executed immediately")
+        try #require(isExecuted == [true, true, true, false], "Expect first 2 items to be executed immediately")
     }
-    
-    @ImagePipelineActor
-    func testOverflow() {
-        // Given
-        var isExecuted = Array(repeating: false, count: 3)
-        
-        // When
-        let expectation = self.expectation(description: "All work executed")
-        expectation.expectedFulfillmentCount = isExecuted.count
-        
-        for i in isExecuted.indices {
-            rateLimiter.execute {
-                isExecuted[i] = true
-                expectation.fulfill()
-                return true
+
+    @Test func overflow() async throws {
+        let count = 3
+        await confirmation(expectedCount: count) { done in
+            for _ in 0..<count {
+                await withUnsafeContinuation { continuation in
+                    rateLimiter.execute {
+                        done()
+                        continuation.resume(returning: ())
+                        return true
+                    }
+                }
             }
         }
-        
-        // When time is passed
-        wait()
-        
-        // Then
-        XCTAssertEqual(isExecuted, [true, true, true], "Expect 3rd item to be executed after a short delay")
     }
 }
