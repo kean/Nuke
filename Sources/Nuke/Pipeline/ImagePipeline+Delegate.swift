dfd86bdb04741c4c8abea821d11ab24108f0602a
kean <grebenyuk.alexander@gmail.com>
2025-01-01 14:20:07 -0500

    Update copyright

--- Sources/Nuke/Pipeline/ImagePipeline+Delegate.swift
+++ Sources/Nuke/Pipeline/ImagePipeline+Delegate.swift
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 //
-// Copyright (c) 2015-2024 Alexander Grebenyuk (github.com/kean).
+// Copyright (c) 2015-2025 Alexander Grebenyuk (github.com/kean).
 
 import Foundation
 
