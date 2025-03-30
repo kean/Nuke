f026eadd624d76742aa98288cd967a7325bc5d22
kean <grebenyuk.alexander@gmail.com>
2025-03-30 12:45:35 -0400

    Update Host

--- Tests/Host/AppDelegate.swift
+++ Tests/Host/AppDelegate.swift
@@ -4,8 +4,19 @@
 
 import UIKit
 
-@UIApplicationMain
+@main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
+}
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 }
