54fc580fa324cc9131e901a14d59b0bc379a036c
Peter Kovacs <kope8899@gmail.com>
2025-08-14 18:09:03 +0200

    - bump deployment target to iOS 16 - bump deployment target to tvOS 15

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ import PackageDescription
 let package = Package(
     name: "Nuke",
     platforms: [
-        .iOS(.v13),
-        .tvOS(.v13),
+        .iOS(.v16),
+        .tvOS(.v15),
         .macOS(.v10_15),
         .watchOS(.v6),
         .visionOS(.v1),
