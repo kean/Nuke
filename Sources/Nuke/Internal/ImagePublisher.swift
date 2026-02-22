b51915123fe8257c08b7749eacc1848224b1f124
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2026-02-22 16:39:01 -0500

    Update Copyright

--- Sources/Nuke/Internal/ImagePublisher.swift
+++ Sources/Nuke/Internal/ImagePublisher.swift
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 //
-// Copyright (c) 2020-2024 Alexander Grebenyuk (github.com/kean).
+// Copyright (c) 2020-2026 Alexander Grebenyuk (github.com/kean).
 
 import Foundation
 import Combine
