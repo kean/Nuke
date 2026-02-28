e28c384bd7fd219a8413efa80ec345b8d434aa41
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2026-02-27 23:00:31 -0500

    Simplify app host

--- Tests/Host/AppDelegate.swift
+++ Tests/Host/AppDelegate.swift
@@ -6,6 +6,12 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        window = UIWindow()
+        window?.rootViewController = UIViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
 }
