a11cba81404f0e025ca826b48b10b88e7234d2ed
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2026-09-23 18:38:30 -0400

    Turn on isUsingPrepareForDisplay by default

--- Sources/Nuke/Internal/Graphics.swift
+++ Sources/Nuke/Internal/Graphics.swift
@@ -160,8 +160,8 @@ extension PlatformImage {
     /// Decompresses the input image by drawing in the `CGContext`.
     func decompressed(isUsingPrepareForDisplay: Bool) -> PlatformImage? {
 #if os(iOS) || os(tvOS) || os(visionOS)
-        if isUsingPrepareForDisplay {
-            return preparingForDisplay()
+        if isUsingPrepareForDisplay, let image = preparingForDisplay() {
+            return image
         }
 #endif
         guard let cgImage else {
